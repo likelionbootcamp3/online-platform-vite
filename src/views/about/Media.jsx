ccc223b92efcad4da027c645ae4ebcf4907efebf
likelionbootcamp3 <likelionbootcamp3@gmail.com>
2023-01-19 12:16:03 +0700

    fix: update media

--- src/views/about/Media.jsx
+++ src/views/about/Media.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import Container from "../../components/common/Container";
 
 const mediaData = [
@@ -11,7 +12,7 @@ const mediaData = [
       description:
         "Likelion, a programming education brand, we will host 'MapleStory Worlds X Super Hackathon 2022' with global game company NEXON. ",
       author: "Edaily",
-      date: "2022.08.18",
+      date: new Date("2022-03-25"),
     },
   },
   {
@@ -23,7 +24,7 @@ const mediaData = [
       description:
         "Likelion (CEO Lee Doo-hee) announced on the 19th that the domestic non-fungible token (NFT) project 'SYLTARE' was globally launched on the 18th. ",
       author: "Electronic Newspaper ",
-      date: "2022.08.19",
+      date: new Date("2022-03-25"),
     },
   },
   {
@@ -36,7 +37,7 @@ const mediaData = [
       description:
         "Hyundai Card and global programming education brand 'Like a Stylish Lion' announced on the 7th that they would establish a joint venture (JV) to promote a new NFT (Non-Fungible Token) business.",
       author: "Herald Economy",
-      date: "2022.06.07",
+      date: new Date("2022-03-25"),
     },
   },
   {
@@ -48,35 +49,31 @@ const mediaData = [
       description:
         "Take a look at the Meta Kongz Holder Party, from entry to exit",
       author: "CoinDesk Korea",
-      date: "2022.06.12",
+      date: new Date("2022-03-25"),
     },
   },
 ];
 
-const MediaInfo = ({ media }) => {
+const MediaCard = ({ title, url, brand, description, author, date }) => {
   return (
     <li>
       <a href="#" className="flex h-full flex-col bg-[#F5F5F5]">
         <div className="aspect-[227/141]">
           {/* Image */}
-          <img
-            src={media.snippet.url}
-            alt={media.snippet.brand}
-            className="object-fit h-full w-full"
-          />
+          <img src={url} alt={brand} className="object-fit h-full w-full" />
         </div>
         <div className="flex flex-grow flex-col py-3 px-4">
-          {/* Title Item */}
+          {/* Title */}
           <div className="flex-grow md:flex-grow-0">
             <h4 className="text-sm font-semibold leading-normal md:text-lg">
-              {media.snippet.title}
+              {title}
             </h4>
           </div>
 
-          {/* Detail Item */}
+          {/* Description */}
           <div className="flex-grow">
             <p className="mt-1 hidden text-base text-[#737373] md:line-clamp-3">
-              {media.snippet.description}
+              {description}
             </p>
           </div>
 
@@ -85,10 +82,10 @@ const MediaInfo = ({ media }) => {
           {/* Author and Date */}
           <div className="flex flex-col text-xs text-[#A3A3A3] md:flex-row md:items-center">
             <div className="flex items-center">
-              {media.snippet.author}
+              {author}
               <span className="mx-1 h-2.5 w-px bg-[#A3A3A3]"></span>
             </div>
-            <span>{media.snippet.date}</span>
+            <span>{moment(date).format("YYYY.MM.DD")}</span>
           </div>
         </div>
       </a>
@@ -100,7 +97,6 @@ const Media = () => {
   return (
     <section>
       <Container>
-        {/* TODO: CREATE MEDIA SECTION */}
         <div className="py-20">
           {/* Title */}
           <div className="md:text-center">
@@ -116,9 +112,9 @@ const Media = () => {
           {/* Media Grid */}
           <ul className="mt-12 grid w-full grid-cols-2 gap-4 md:gap-6 lg:grid-cols-4">
             {/* TODO: Map media data here! */}
-            {mediaData.map((item) => (
-              <MediaInfo key={item.id} media={item} />
-            ))}
+            {mediaData.map((media) => {
+              return <MediaCard key={media.id} {...media.snippet} />;
+            })}
           </ul>
         </div>
       </Container>

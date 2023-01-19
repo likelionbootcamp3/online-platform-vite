b2e268b4f15f3b640b9bbb29951a66c8af5bd3cc
Fugena22 <119840033+Fugena22@users.noreply.github.com>
2023-01-19 12:02:06 +0700

    fix: update media

--- src/views/about/Media.jsx
+++ src/views/about/Media.jsx
@@ -3,46 +3,98 @@ import Container from "../../components/common/Container";
 const mediaData = [
   {
     id: 1,
-    title:
-      "Likelion, holding a hackathon based on Nexon and content making platform",
-    imageSrc: "",
-    brand: "Likelion",
-    description:
-      "Likelion, a programming education brand, we will host 'MapleStory Worlds X Super Hackathon 2022' with global game company NEXON.",
-    author: "",
-    date: "",
+    snippet: {
+      title:
+        "Likelion, holding a hackathon based on Nexon and content making platform",
+      url: "https://www.likelion.net/img/media_img1.png",
+      brand: "Techit",
+      description:
+        "Likelion, a programming education brand, we will host 'MapleStory Worlds X Super Hackathon 2022' with global game company NEXON. ",
+      author: "Edaily",
+      date: "2022.08.18",
+    },
   },
   {
     id: 2,
-    title:
-      "Likelion, holding a hackathon based on Nexon and content making platform",
-    imageSrc: "",
-    description:
-      "Likelion, a programming education brand, we will host 'MapleStory Worlds X Super Hackathon 2022' with global game company NEXON.",
-    author: "",
-    date: "",
+    snippet: {
+      title: "Likelion, global launch of NFT project 'Thread'",
+      url: "https://www.likelion.net/img/media_img2.png",
+      brand: "Styltare",
+      description:
+        "Likelion (CEO Lee Doo-hee) announced on the 19th that the domestic non-fungible token (NFT) project 'SYLTARE' was globally launched on the 18th. ",
+      author: "Electronic Newspaper ",
+      date: "2022.08.19",
+    },
   },
   {
     id: 3,
-    title:
-      "Likelion, holding a hackathon based on Nexon and content making platform",
-    imageSrc: "",
-    description:
-      "Likelion, a programming education brand, we will host 'MapleStory Worlds X Super Hackathon 2022' with global game company NEXON.",
-    author: "",
-    date: "",
+    snippet: {
+      title:
+        "'Hyundai Card-Likelion', established a joint venture for NFT business",
+      url: "https://www.likelion.net/img/media_img3.png",
+      brand: "Modern Lion",
+      description:
+        "Hyundai Card and global programming education brand 'Like a Stylish Lion' announced on the 7th that they would establish a joint venture (JV) to promote a new NFT (Non-Fungible Token) business.",
+      author: "Herald Economy",
+      date: "2022.06.07",
+    },
   },
   {
     id: 4,
-    title:
-      "Likelion, holding a hackathon based on Nexon and content making platform",
-    imageSrc: "",
-    description:
-      "Likelion, a programming education brand, we will host 'MapleStory Worlds X Super Hackathon 2022' with global game company NEXON.",
-    author: "",
-    date: "",
+    snippet: {
+      title: "Take a look at the Meta Kongz Holder Party, from entry to exit",
+      url: "https://www.likelion.net/img/media_img4.png",
+      brand: "Meta Kongz",
+      description:
+        "Take a look at the Meta Kongz Holder Party, from entry to exit",
+      author: "CoinDesk Korea",
+      date: "2022.06.12",
+    },
   },
 ];
+
+const MediaInfo = ({ media }) => {
+  return (
+    <li>
+      <a href="#" className="flex h-full flex-col bg-[#F5F5F5]">
+        <div className="aspect-[227/141]">
+          {/* Image */}
+          <img
+            src={media.snippet.url}
+            alt={media.snippet.brand}
+            className="object-fit h-full w-full"
+          />
+        </div>
+        <div className="flex flex-grow flex-col py-3 px-4">
+          {/* Title Item */}
+          <div className="flex-grow md:flex-grow-0">
+            <h4 className="text-sm font-semibold leading-normal md:text-lg">
+              {media.snippet.title}
+            </h4>
+          </div>
+
+          {/* Detail Item */}
+          <div className="flex-grow">
+            <p className="mt-1 hidden text-base text-[#737373] md:line-clamp-3">
+              {media.snippet.description}
+            </p>
+          </div>
+
+          <hr className="my-2 border-[#D4D4D4]" />
+
+          {/* Author and Date */}
+          <div className="flex flex-col text-xs text-[#A3A3A3] md:flex-row md:items-center">
+            <div className="flex items-center">
+              {media.snippet.author}
+              <span className="mx-1 h-2.5 w-px bg-[#A3A3A3]"></span>
+            </div>
+            <span>{media.snippet.date}</span>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+};
 
 const Media = () => {
   return (
@@ -64,46 +116,9 @@ const Media = () => {
           {/* Media Grid */}
           <ul className="mt-12 grid w-full grid-cols-2 gap-4 md:gap-6 lg:grid-cols-4">
             {/* TODO: Map media data here! */}
-            <li>
-              <a href="#" className="flex h-full flex-col bg-[#F5F5F5]">
-                <div className="aspect-[227/141]">
-                  {/* Image */}
-                  <img
-                    src="https://kiranworkspace.com/wp-content/uploads/2020/12/ellipsis-multiline-css.png"
-                    alt=""
-                    className="object-fit h-full w-full"
-                  />
-                </div>
-                <div className="flex flex-grow flex-col py-3 px-4">
-                  {/* Title Item */}
-                  <div className="flex-grow md:flex-grow-0">
-                    <h4 className="text-sm font-semibold leading-normal md:text-lg">
-                      Likelion, holding a hackathon based
-                    </h4>
-                  </div>
-
-                  {/* Detail Item */}
-                  <div className="flex-grow">
-                    <p className="mt-1 hidden text-base text-[#737373] md:line-clamp-3">
-                      Likelion, a programming education brand, we will host
-                      'MapleStory Worlds X Super Hackathon 2022' with global
-                      game company NEXON.
-                    </p>
-                  </div>
-
-                  <hr className="my-2 border-[#D4D4D4]" />
-
-                  {/* Author and Date */}
-                  <div className="flex flex-col text-xs text-[#A3A3A3] md:flex-row md:items-center">
-                    <div className="flex items-center">
-                      Edaily
-                      <span className="mx-1 h-2.5 w-px bg-[#A3A3A3]"></span>
-                    </div>
-                    <span>2022.08.18</span>
-                  </div>
-                </div>
-              </a>
-            </li>
+            {mediaData.map((item) => (
+              <MediaInfo key={item.id} media={item} />
+            ))}
           </ul>
         </div>
       </Container>

8bd7c93c2c7ae7c347211b29c401b50b49c2bcf3
likelionbootcamp3 <likelionbootcamp3@gmail.com>
2023-01-30 10:39:13 +0700

    Create Mission.jsx

---
+++ src/views/about/Mission.jsx
@@ -0,0 +1,16 @@
+import Container from "../../components/common/Container";
+
+const Mission = () => {
+  return (
+    <section>
+      <Container>
+        <div className="py-20">
+          {/* TODO: CREATE MISSION SECTION */}
+          <div></div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Mission;

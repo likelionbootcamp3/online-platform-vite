2f88acd49628c2adfdde44b06b9837ee1cfc835f
likelionbootcamp3 <likelionbootcamp3@gmail.com>
2023-01-30 13:51:37 +0700

    Update Mission.jsx

--- src/views/about/Mission.jsx
+++ src/views/about/Mission.jsx
@@ -2,22 +2,19 @@ import Container from "../../components/common/Container";
 
 const Mission = () => {
   return (
-    <section>
+    <section className="bg-orange-500">
       <Container>
-        <div className="py-20">
-          {/* TODO: CREATE MISSION SECTION */}
-          <div className="flex h-80 flex-col items-center justify-center bg-orange-500 md:h-96 lg:h-[36rem]">
-            <p className="mb-4 text-center text-sm font-bold md:mb-5 md:text-base lg:mb-6 lg:text-lg">
-              BRAND MISSION
-            </p>
-            <h2 className="px-10 text-center text-base font-semibold md:px-36 md:text-lg lg:px-52 lg:text-2xl">
-              Chúng tôi góp phần cùng bạn kiến tạo nên ngày hôm nay gần nhất với
-              tương lai của bạn. Xuất phát từ nền tảng kinh nghiệm trong giáo
-              dục công nghệ dành cho những người dám ước mơ, kết hợp cùng năng
-              lực của LIKELION hứa hẹn sẽ cùng bạn tạo nên một cộng đồng và hệ
-              sinh thái Web3 đầy tiềm lực.
-            </h2>
-          </div>
+        <div className="flex h-80 flex-col items-center justify-center md:h-96 lg:h-[36rem]">
+          <p className="mb-4 text-center text-sm font-bold md:mb-5 md:text-base lg:mb-6 lg:text-lg">
+            BRAND MISSION
+          </p>
+          <h2 className="px-10 text-center text-base font-semibold md:px-36 md:text-lg lg:px-52 lg:text-2xl">
+            Chúng tôi góp phần cùng bạn kiến tạo nên ngày hôm nay gần nhất với
+            tương lai của bạn. Xuất phát từ nền tảng kinh nghiệm trong giáo dục
+            công nghệ dành cho những người dám ước mơ, kết hợp cùng năng lực của
+            LIKELION hứa hẹn sẽ cùng bạn tạo nên một cộng đồng và hệ sinh thái
+            Web3 đầy tiềm lực.
+          </h2>
         </div>
       </Container>
     </section>

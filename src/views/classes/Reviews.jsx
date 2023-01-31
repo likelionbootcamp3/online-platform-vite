cae5bfd9591d782c4c8de851eb0995592f4e3671
likelionbootcamp3 <likelionbootcamp3@gmail.com>
2023-01-31 09:23:25 +0700

    set up reviews

---
+++ src/views/classes/Reviews.jsx
@@ -0,0 +1,18 @@
+import Container from "../../components/common/Container";
+
+// TODO: CREATE REVIEW DATA
+
+// TODO: CREATE REVIEW CARD
+
+const Reviews = () => {
+  return (
+    <div>
+      <Container>
+        {/* TODO: CREATE REVIEW GRID */}
+        <div></div>
+      </Container>
+    </div>
+  );
+};
+
+export default Reviews;
--- src/views/classes/index.jsx
+++ src/views/classes/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./Banner";
+import Reviews from "./Reviews";
 import Videos from "./Videos";
 
 const Classes = () => {
@@ -7,6 +8,7 @@ const Classes = () => {
     <div>
       <Banner />
       <Videos />
+      <Reviews />
     </div>
   );
 };

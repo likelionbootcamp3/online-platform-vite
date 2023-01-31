506b88f204482c0240ec6e6f3acd0cfa1b5dc269
Fugena22 <tuanphucnguyen2110@gmail.com>
2023-01-31 16:30:51 +0700

    feat: create reviews

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import router from "./config/router";
 import { RouterProvider } from "react-router-dom";
+import "swiper/css";
+import "swiper/css/navigation";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
